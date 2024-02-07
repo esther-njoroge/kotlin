5272b0e4d87a2c8ad328e57cef4b4648dc857bbe
esther-njoroge <nyokabinjorogeesther@gmail.com>
2024-02-07 23:29:05 +0300

    added one file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(){
+    var fullName: String = "June Smith"
+    println(fullName)
+    var age: Byte = 21
+    println(age)
+    val phoneNumber: String = "0712324563"
+    println(phoneNumber)
+    var weight: Float = 61F
+    println(weight)
+    var citizenship: Boolean = true
+    println(true)
+
+
+
+
+}
+
